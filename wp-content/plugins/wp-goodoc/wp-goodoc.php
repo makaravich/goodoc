f3b559ea12ef12469e568c907873752cf5809873
makaravich <d.makarski@gmail.com>
2023-12-23 20:19:41 +0100

    Add empty plugin

---
+++ wp-content/plugins/wp-goodoc/wp-goodoc.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * WP GooDoc
+ *
+ * @package       GOODOC
+ * @author        Dzmitry Makarski
+ * @license       gplv2
+ * @version       0.0.1
+ *
+ * @wordpress-plugin
+ * Plugin Name:   WP GooDoc
+ * Plugin URI:    https://example.com
+ * Description:   Plugin to import Google Documents into a WordPress site
+ * Version:       0.0.1
+ * Author:        Dzmitry Makarski
+ * Author URI:    https://example.com
+ * Text Domain:   wp-goodoc
+ * Domain Path:   /languages
+ * License:       GPLv2
+ * License URI:   https://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with WP GooDoc. If not, see <https://www.gnu.org/licenses/gpl-2.0.html/>.
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * HELPER COMMENT START
+ * 
+ * This file contains the logic required to run the plugin.
+ * To add some functionality, you can simply define the WordPres hooks as followed: 
+ * 
+ * add_action( 'init', 'some_callback_function', 10 );
+ * 
+ * and call the callback function like this 
+ * 
+ * function some_callback_function(){}
+ * 
+ * HELPER COMMENT END
+ */
+
+// Include your custom code here.
